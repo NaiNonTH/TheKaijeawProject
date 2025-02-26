2331a910461b3f7f4d4327f7c132fd76d82b6c42
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-02-26 22:42:06 +0700

    ไม่น่า push แบบไม่ test เลยกู้วววว

--- TheKaijeawProject/urls.py
+++ TheKaijeawProject/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("orders.urls")),
+    path('', include("app.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
