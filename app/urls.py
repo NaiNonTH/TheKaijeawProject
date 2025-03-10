14f348d516b3cdf4e72c26514e3ecf1161adf048
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-03-10 23:26:31 +0700

    แก้หน้าเปิดปิดเมนูให้เหลือปุ่มเดียว + ให้ redirect ไปที่หน้าคำสั่งซื้อหลังเปิดปิดเมนูเสร็จเลย

--- app/urls.py
+++ app/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path('restaurant/orders', views.orders_page, name="orders_page"),
     path('restaurant/orders/mark-as-done', views.mark_order_as_done, name="mark_order_as_done"),
     path('restaurant/menus', views.restaurant_menu_page, name="restaurant_menu_page"),
-    path('restaurant/menus/update-filling-availability', views.update_filling_availability, name="update_filling_availability"),
-    path('restaurant/menus/toggle-takeaway', views.toggle_takeaway, name="toggle_takeaway"),
-    path('restaurant/menus/toggle-restaurant', views.toggle_restaurant, name="toggle_restaurant"),
+    path('restaurant/menus/update-status', views.update_status, name="update_status"),
     path('restaurant/statistics', views.statistics_page, name="statistics_page")
 ]
